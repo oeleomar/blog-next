76e6de2d669f5a61ebbf59c5dea872e39073a5a0
oeleomar <eleomar.dev@gmail.com>
2024-11-06 22:36:49 -0400

    chore: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import { LogInIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import Image from "next/image";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+  
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="m-auto flex max-w-[400px] flex-col justify-center rounded-2xl border p-8 shadow-md">
@@ -19,10 +28,12 @@ const LoginPage = () => {
           personalizar sua experiência conosco, utilize o botão abaixo para
           fazer login ou cadastrar-se. Sua jornada começa aqui.
         </p>
-        <Button variant="outline">
-          <LogInIcon />
-          Faça o login ou se cadastre.
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon />
+            Faça o login ou se cadastre.
+          </Button>
+        </SignInButton>
       </div>
     </div>
   );
